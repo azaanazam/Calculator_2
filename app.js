3be225bee898a76252fc4afd40f2a14039c9ce9b
Azan Azam <168602523+azaanazam@users.noreply.github.com>
2025-10-29 00:40:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let del = document.getElementById("del")
+let arro = document.getElementById("arro")
+let per = document.getElementById("per")
+let divi = document.getElementById("divi")
+let mul = document.getElementById("mul")
+let add = document.getElementById("add")
+let cir = document.getElementById("cir")
+let ope = document.getElementById("ope")
+document.getElementById("input").disabled = true;
+
+
+// function sev (){
+// let sev = document.getElementById("sev").value
+// let eig = document.getElementById("eig")
+// let nin = document.getElementById("nin")
+// let four = document.getElementById("four")
+// let fiv = document.getElementById("fiv")
+// let six = document.getElementById("six")
+// let sub = document.getElementById("sub")
+// let one = document.getElementById("one")
+// let two = document.getElementById("two")
+// let three = document.getElementById("three")
+// let zero = document.getElementById("zero")
+// let zero2 = document.getElementById("zero2")
+
+// }
+
+// sev()
+
+const input = document.getElementById('input');
+const num = document.getElementById('num')
+function sev(){
+    input.value = num.value;
+}
+
+
+
+// function my(){
+//     let sev = document.getElementById("sev").value;
+//     input.value = sev.value;
+//     console.log(sev.innerText,sev.value)
+
+// }
